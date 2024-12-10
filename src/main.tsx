4ccdcd1fdf9e67775375467a6ef0f30eaea18892
fdg312 <ffedorovzx@gmail.com>
2024-12-10 21:36:17 +0500

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,17 @@
 import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Navigate, Route, Routes } from 'react-router'
+import { HashRouter, Navigate, Route, Routes } from 'react-router'
 import App from './App.tsx'
 import './index.css'
 import CreateProduct from './pages/createProduct.tsx'
 import Product from './pages/Product.tsx'
 
 createRoot(document.getElementById('root')!).render(
-	<BrowserRouter basename='/test_alpha_project'>
+	<HashRouter basename='/test_alpha_project'>
 		<Routes>
 			<Route path='/' element={<Navigate to='/products' replace />} />
 			<Route path='/products' element={<App />} />
 			<Route path='/create-product' element={<CreateProduct />} />
 			<Route path='/products/:id' element={<Product />} />
 		</Routes>
-	</BrowserRouter>
+	</HashRouter>
 )
