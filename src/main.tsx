d9832ba710b5bf7a52646ae0e9b6e34f17212cfb
fdg312 <ffedorovzx@gmail.com>
2024-12-10 21:53:14 +0500

    For deployment

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,7 @@ import Product from './pages/Product.tsx'
 createRoot(document.getElementById('root')!).render(
 	<BrowserRouter basename='/test_alpha_project/'>
 		<Routes>
-			<Route path='/' element={<Navigate to='/products' replace />} />
+			<Route path='*' element={<Navigate to='/products' replace />} />
 			<Route path='/products' element={<App />} />
 			<Route path='/create-product' element={<CreateProduct />} />
 			<Route path='/products/:id' element={<Product />} />
